494f06caa0bd49425fcc3de811f9e6fa5ea0bec8
Bruno Turcksin <bruno.turcksin@gmail.com>
2016-07-19 17:35:06 -0400

    Add input file example.

--- application/adamantine.cc
+++ application/adamantine.cc
@@ -44,23 +44,22 @@ int main(int argc, char *argv[])
     }
 
     // Read the input.
-    adamantine::ASSERT_THROW(map.count("input-file") == 0,
-                             "Input file not found.");
     std::string const filename = map["input-file"].as<std::string>();
     adamantine::ASSERT_THROW(boost::filesystem::exists(filename) == true,
                              "The file " + filename + " does not exist.");
     boost::property_tree::ptree database;
     boost::property_tree::info_parser::read_info(filename, database);
 
-    int const dim = database.get<int>("dim");
+    boost::property_tree::ptree geometry_database = database.get_child("geometry");
+    int const dim = geometry_database.get<int>("dim");
     adamantine::ASSERT_THROW((dim == 2) || (dim == 3), "dim should be 2 or 3");
     if (dim == 2)
     {
-      adamantine::Geometry<2> geometry(communicator, database);
+      adamantine::Geometry<2> geometry(communicator, geometry_database);
     }
     else
     {
-      adamantine::Geometry<2> geometry(communicator, database);
+      adamantine::Geometry<2> geometry(communicator, geometry_database);
     }
   }
   catch (std::exception &exception)
