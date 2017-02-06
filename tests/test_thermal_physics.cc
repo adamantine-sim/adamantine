3e04f10ef9c82752ae0d259cc3f0f7bd298db3dc
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-02-06 14:22:08 -0500

    Fix a bug when initializing the temperature and improve test.

--- tests/test_thermal_physics.cc
+++ tests/test_thermal_physics.cc
@@ -133,3 +133,48 @@ BOOST_AUTO_TEST_CASE(thermal_2d_manufactured_solution)
   for (unsigned int i = 0; i < solution.size(); ++i)
     BOOST_CHECK_CLOSE(solution[i], 0.1, tolerance);
 }
+
+BOOST_AUTO_TEST_CASE(initial_temperature)
+{
+  boost::mpi::communicator communicator;
+
+  // Geometry database
+  boost::property_tree::ptree geometry_database;
+  geometry_database.put("length", 12e-3);
+  geometry_database.put("length_divisions", 1);
+  geometry_database.put("height", 6e-3);
+  geometry_database.put("height_divisions", 1);
+  // Build Geometry
+  adamantine::Geometry<2> geometry(communicator, geometry_database);
+  boost::property_tree::ptree database;
+  // Material property
+  database.put("materials.n_materials", 1);
+  database.put("materials.material_0.solid.density", 1.);
+  database.put("materials.material_0.powder.density", 10.);
+  database.put("materials.material_0.liquid.density", 1.);
+  database.put("materials.material_0.solid.specific_heat", 1.);
+  database.put("materials.material_0.powder.specific_heat", 2.);
+  database.put("materials.material_0.liquid.specific_heat", 1.);
+  database.put("materials.material_0.solid.thermal_conductivity", 1.);
+  database.put("materials.material_0.powder.thermal_conductivity", 1.);
+  database.put("materials.material_0.liquid.thermal_conductivity", 1.);
+  // Source database
+  database.put("sources.n_beams", 1);
+  database.put("sources.beam_0.depth", 1e100);
+  database.put("sources.beam_0.energy_conversion_efficiency", 0.1);
+  database.put("sources.beam_0.control_efficiency", 1.0);
+  database.put("sources.beam_0.diameter", 1e100);
+  database.put("sources.beam_0.max_power", 1e300);
+  database.put("sources.beam_0.abscissa", "t");
+  // Time-stepping database
+  database.put("time_stepping.method", "rk_fourth_order");
+  // Build ThermalPhysics
+  adamantine::ThermalPhysics<2, 2, double, dealii::QGauss<1>> physics(
+      communicator, database, geometry);
+  physics.setup_dofs();
+  physics.reinit();
+
+  dealii::LA::distributed::Vector<double> solution;
+  physics.initialize_dof_vector(1000., solution);
+  BOOST_CHECK(solution.l1_norm() == 20000. * solution.size());
+}
