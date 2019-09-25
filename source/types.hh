db63338e7e367010f651e4cafcda5244c620c5f7
Bruno Turcksin <bruno.turcksin@gmail.com>
2019-09-25 09:30:04 -0400

    Update copyright year

--- source/types.hh
+++ source/types.hh
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016 - 2017, the adamantine authors.
+/* Copyright (c) 2016 - 2019, the adamantine authors.
  *
  * This file is subject to the Modified BSD License and may not be distributed
  * without copyright and license information. Please refer to the file LICENSE
