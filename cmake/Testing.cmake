bf0530c47cf92bd5108c66add7ae26d6f872df06
Bruno Turcksin <bruno.turcksin@gmail.com>
2020-05-07 21:26:48 +0000

    Add CMake infrastructure for CUDA

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -10,8 +10,44 @@ function(adamantine_ADD_BOOST_TEST TEST_NAME)
     target_link_libraries(${TEST_NAME} MPI::MPI_CXX)
     target_link_libraries(${TEST_NAME} Adamantine)
     set_target_properties(${TEST_NAME} PROPERTIES
-        CXX_STANDARD 17
+        CXX_STANDARD 14
         CXX_STANDARD_REQUIRED ON
+        CXX_EXTENSIONS OFF
+    )
+    if(ARGN)
+        set(NUMBER_OF_PROCESSES_TO_EXECUTE ${ARGN})
+    else()
+        set(NUMBER_OF_PROCESSES_TO_EXECUTE 1)
+    endif()
+    foreach(NPROC ${NUMBER_OF_PROCESSES_TO_EXECUTE})
+        add_test(
+            NAME ${TEST_NAME}_${NPROC}
+            COMMAND ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${NPROC} ${CMAKE_BINARY_DIR}/bin/${TEST_NAME}
+        )
+        set_tests_properties(${TEST_NAME}_${NPROC} PROPERTIES
+            PROCESSORS ${NPROC}
+        )
+    endforeach()
+endfunction()
+
+function(adamantine_ADD_BOOST_CUDA_TEST TEST_NAME)
+    add_executable(${TEST_NAME} ${CMAKE_CURRENT_SOURCE_DIR}/${TEST_NAME}.cu ${tests_SOURCES})
+    DEAL_II_SETUP_TARGET(${TEST_NAME})
+    target_link_libraries(${TEST_NAME} Boost::boost)
+    target_link_libraries(${TEST_NAME} Boost::chrono)
+    target_link_libraries(${TEST_NAME} Boost::filesystem)
+    target_link_libraries(${TEST_NAME} Boost::program_options)
+    target_link_libraries(${TEST_NAME} Boost::timer)
+    target_link_libraries(${TEST_NAME} Boost::unit_test_framework)
+    target_link_libraries(${TEST_NAME} MPI::MPI_CXX)
+    target_link_libraries(${TEST_NAME} Adamantine)
+    set_target_properties(${TEST_NAME} PROPERTIES
+        CXX_STANDARD 14
+        CXX_STANDARD_REQUIRED ON
+        CXX_EXTENSIONS OFF
+        CUDA_SEPARABLE_COMPILATION ON
+        CUDA_STANDARD 14
+        CUDA_STANDARD_REQUIRED ON
     )
     if(ARGN)
         set(NUMBER_OF_PROCESSES_TO_EXECUTE ${ARGN})
