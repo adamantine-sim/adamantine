1f0d7576093c9704cb984f4a0045246b0a7a70e9
Bruno Turcksin <bruno.turcksin@gmail.com>
2020-12-03 18:10:58 +0000

    Update to compile with new version of deal.II

--- tests/test_implicit_operator.cc
+++ tests/test_implicit_operator.cc
@@ -39,7 +39,7 @@ BOOST_AUTO_TEST_CASE(implicit_operator)
   dealii::hp::FECollection<2> fe_collection;
   fe_collection.push_back(dealii::FE_Q<2>(2));
   fe_collection.push_back(dealii::FE_Nothing<2>());
-  dealii::DoFHandler<2> dof_handler(geometry.get_triangulation(), true);
+  dealii::DoFHandler<2> dof_handler(geometry.get_triangulation());
   dof_handler.distribute_dofs(fe_collection);
   dealii::AffineConstraints<double> affine_constraints;
   affine_constraints.close();
