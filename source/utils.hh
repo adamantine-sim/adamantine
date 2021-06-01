61b423e204451c199bdaf6a0bf508c8ee40bc159
Bruno Turcksin <bruno.turcksin@gmail.com>
2021-06-01 18:30:23 +0000

    Add ArrayMD data structure as a host/device data structure

--- source/utils.hh
+++ source/utils.hh
@@ -8,13 +8,75 @@
 #ifndef UTILS_HH
 #define UTILS_HH
 
+#include <deal.II/base/cuda.h>
+#include <deal.II/base/exceptions.h>
+
 #include <cassert>
+#include <cstring>
 #include <exception>
 #include <stdexcept>
 #include <string>
 
 namespace adamantine
 {
+#ifdef __CUDACC__
+#define ADAMANTINE_HOST_DEV __host__ __device__
+#else
+#define ADAMANTINE_HOST_DEV
+#endif
+
+template <typename Number, typename MemorySpaceType>
+struct Memory
+{
+  static Number *allocate_data(std::size_t const size);
+
+  static void delete_data(Number *data_ptr) noexcept;
+
+  static void set_zero(Number *data_ptr, std::size_t const size);
+};
+
+template <typename Number>
+struct Memory<Number, dealii::MemorySpace::Host>
+{
+  static Number *allocate_data(std::size_t const size)
+  {
+    Number *data_ptr = new Number[size];
+    return data_ptr;
+  }
+
+  static void delete_data(Number *data_ptr) noexcept { delete[] data_ptr; }
+
+  static void set_zero(Number *data_ptr, std::size_t const size)
+  {
+    std::memset(data_ptr, 0, size * sizeof(Number));
+  }
+};
+
+#ifdef __CUDACC__
+template <typename Number>
+struct Memory<Number, dealii::MemorySpace::CUDA>
+{
+  static Number *allocate_data(std::size_t const size)
+  {
+    Number *data_ptr;
+    dealii::Utilities::CUDA::malloc(data_ptr, size);
+    return data_ptr;
+  }
+
+  static void delete_data(Number *data_ptr) noexcept
+  {
+    cudaError_t const error_code = cudaFree(data_ptr);
+    AssertNothrowCuda(error_code);
+  }
+
+  static void set_zero(Number *data_ptr, std::size_t const size)
+  {
+    cudaError_t const error_code =
+        cudaMemset(data_ptr, 0, size * sizeof(Number));
+    AssertCuda(error_code);
+  }
+};
+#endif
 
 inline void ASSERT(bool cond, std::string const &message)
 {
