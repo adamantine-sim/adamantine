94562699afd7cddc151e8883e442dcbfc34f5cf7
Bruno Turcksin <bruno.turcksin@gmail.com>
2022-04-12 15:06:28 +0000

    Make a test more robust

--- tests/test_experimental_data.cc
+++ tests/test_experimental_data.cc
@@ -143,8 +143,8 @@ BOOST_AUTO_TEST_CASE(read_experimental_data_ray_tracing_from_file)
   // Reference solution
   std::vector<double> values_ref = {1, 2, 3, 5};
   std::vector<dealii::Point<3>> points_ref;
-  points_ref.emplace_back(0., 0., 0.);
-  points_ref.emplace_back(1., 0., 0.);
+  points_ref.emplace_back(0., 0.1, 0.2);
+  points_ref.emplace_back(1., 0.1, 0.);
   points_ref.emplace_back(1., 0.5, 0.);
   points_ref.emplace_back(1., 0.5, 0.5);
 
