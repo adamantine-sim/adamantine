c7bfefa734c910442407f22cd0c65fd664a81117
Bruno Turcksin <bruno.turcksin@gmail.com>
2022-11-15 20:58:31 +0000

    Remove duplicated test

--- tests/test_integration_da_augmented.cc
+++ tests/test_integration_da_augmented.cc
@@ -50,29 +50,3 @@ BOOST_AUTO_TEST_CASE(integration_3D_data_assimilation_augmented,
   double gold_solution = 0.3;
   BOOST_TEST(average_value == gold_solution);
 }
-
-BOOST_AUTO_TEST_CASE(integration_3D, *utf::tolerance(0.1))
-{
-  MPI_Comm communicator = MPI_COMM_WORLD;
-
-  std::vector<adamantine::Timer> timers;
-  initialize_timers(communicator, timers);
-
-  // Read the input.
-  std::string const filename = "demo_316_short_anisotropic.info";
-  adamantine::ASSERT_THROW(boost::filesystem::exists(filename) == true,
-                           "The file " + filename + " does not exist.");
-  boost::property_tree::ptree database;
-  boost::property_tree::info_parser::read_info(filename, database);
-
-  auto [temperature, displacement] =
-      run<3, dealii::MemorySpace::Host>(communicator, database, timers);
-
-  std::ifstream gold_file("integration_3d_gold.txt");
-  for (unsigned int i = 0; i < temperature.locally_owned_size(); ++i)
-  {
-    double gold_value = -1.;
-    gold_file >> gold_value;
-    BOOST_TEST(temperature.local_element(i) == gold_value);
-  }
-}
