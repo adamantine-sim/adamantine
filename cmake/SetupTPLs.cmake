e4b71bc84416b7acf04a8b232daa2eedac7b0ef6
Bruno Turcksin <bruno.turcksin@gmail.com>
2023-06-16 14:41:46 +0000

    Remove dealii-weak_forms from the infrastructure

--- cmake/SetupTPLs.cmake
+++ cmake/SetupTPLs.cmake
@@ -27,16 +27,3 @@ if (ADAMANTINE_ENABLE_CALIPER)
   add_compile_definitions(ADAMANTINE_WITH_CALIPER)
   message(STATUS "Found Caliper: ${caliper_INSTALL_PREFIX}")
 endif()
-
-#### dealii-weak_forms #######################################################
-if (ADAMANTINE_ENABLE_DEALII_WEAK_FORMS)
-  # If the user provides a path, make sure that we use that path and nothing
-  # else.
-  if(DEALII_WEAK_FORMS_DIR)
-    find_package(dealii-weak_forms PATHS ${DEALII_WEAK_FORMS_DIR} NO_DEFAULT_PATH)
-  else()
-    find_package(dealii-weak_forms REQUIRED)
-  endif()
-  add_compile_definitions(ADAMANTINE_WITH_DEALII_WEAK_FORMS)
-  message(STATUS "Found dealii-weak_forms: ${WEAK_FORMS_INCLUDE_DIRS}")
-endif()
