0d67bb3a9cb4300b810b1f2ca52525f0fe4b6d40
Bruno Turcksin <bruno.turcksin@gmail.com>
2023-11-20 11:36:19 -0500

    Improve build system

--- cmake/SetupDealII.cmake
+++ cmake/SetupDealII.cmake
@@ -1,15 +1,28 @@
 #### deal.II #################################################################
 find_package(deal.II 9.5 REQUIRED PATHS ${DEAL_II_DIR})
 
-# If deal.II was configured in DebugRelease mode, then if adamantine was configured
-# in Debug mode, we link against the Debug version of deal.II. If adamantine was
-# configured in Release mode, we link against the Release version of eal.II
-string(FIND "${DEAL_II_LIBRARIES}" "general" SINGLE_DEAL_II)
-if (${SINGLE_DEAL_II} EQUAL -1)
-    if(CMAKE_BUILD_TYPE MATCHES "Release")
-        set(DEAL_II_LIBRARIES ${DEAL_II_LIBRARIES_RELEASE})
-    else()
-        set(DEAL_II_LIBRARIES ${DEAL_II_LIBRARIES_DEBUG})
-  endif()
+deal_ii_initialize_cached_variables()
+
+if(NOT DEAL_II_WITH_CXX17)
+  message(FATAL_ERROR "deal.II needs to be configured with C++17 support.")
 endif()
 
+if(NOT DEAL_II_WITH_MPI)
+  message(FATAL_ERROR "deal.II needs to be configured with MPI support.")
+endif()
+
+if(NOT DEAL_II_WITH_ARBORX)
+  message(FATAL_ERROR "deal.II needs to be configured with ArborX support.")
+endif()
+
+if(NOT DEAL_II_ARBORX_WITH_MPI)
+  message(FATAL_ERROR "ArborX needs to be configured with MPI support.")
+endif()
+
+if(NOT DEAL_II_WITH_P4EST)
+  message(FATAL_ERROR "deal.II needs to be configured with P4EST support.")
+endif()
+
+if(NOT DEAL_II_WITH_TRILINOS)
+  message(FATAL_ERROR "deal.II needs to be configured with Trilinos support.")
+endif()
