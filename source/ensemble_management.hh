c0ce7da21e294fd6287fb7be1e992ce685e77f66
Bruno Turcksin <bruno.turcksin@gmail.com>
2024-01-19 19:41:01 +0000

    Add comments

--- source/ensemble_management.hh
+++ source/ensemble_management.hh
@@ -17,7 +17,10 @@ namespace adamantine
 /**
  * Return a vector of size @p length, with random values drawn following a
  * normal distribution of average @p mean and standard deviation @stddev. The
- * first @p n_rejected_draws are rejected.
+ * first @p n_rejected_draws are rejected. @p n_rejected_draws is used to
+ * ensure that wether we use one MPI rank or ten, we use the same random
+ * numbers. If we don't reject the first few draws, all the processors will use
+ * the same "random" numbers since they have the same seed.
  */
 std::vector<double> get_normal_random_vector(unsigned int length,
                                              unsigned int n_rejected_draws,
