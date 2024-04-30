90aac1374424fdbb0fc0b4da70f8dcd79af4c4a5
Daniel Arndt <arndtd@ornl.gov>
2024-04-30 15:50:39 -0400

    Print all deal.II configurations problems before aborting

--- cmake/SetupDealII.cmake
+++ cmake/SetupDealII.cmake
@@ -3,26 +3,37 @@ find_package(deal.II 9.5 REQUIRED PATHS ${DEAL_II_DIR})
 
 deal_ii_initialize_cached_variables()
 
+set(DEAL_II_HAS_ALL_DEPENDENCIES TRUE)
 if(NOT DEAL_II_WITH_CXX17)
-  message(FATAL_ERROR "deal.II needs to be configured with C++17 support.")
+  message(SEND_ERROR "deal.II needs to be configured with C++17 support.")
+  set(DEAL_II_HAS_ALL_DEPENDENCIES FALSE)
 endif()
 
 if(NOT DEAL_II_WITH_MPI)
-  message(FATAL_ERROR "deal.II needs to be configured with MPI support.")
+  message(SEND_ERROR "deal.II needs to be configured with MPI support.")
+  set(DEAL_II_HAS_ALL_DEPENDENCIES FALSE)
 endif()
 
 if(NOT DEAL_II_WITH_ARBORX)
-  message(FATAL_ERROR "deal.II needs to be configured with ArborX support.")
+  message(SEND_ERROR "deal.II needs to be configured with ArborX support.")
+  set(DEAL_II_HAS_ALL_DEPENDENCIES FALSE)
 endif()
 
 if(NOT DEAL_II_ARBORX_WITH_MPI)
-  message(FATAL_ERROR "ArborX needs to be configured with MPI support.")
+  message(SEND_ERROR "ArborX needs to be configured with MPI support.")
+  set(DEAL_II_HAS_ALL_DEPENDENCIES FALSE)
 endif()
 
 if(NOT DEAL_II_WITH_P4EST)
-  message(FATAL_ERROR "deal.II needs to be configured with P4EST support.")
+  message(SEND_ERROR "deal.II needs to be configured with P4EST support.")
+  set(DEAL_II_HAS_ALL_DEPENDENCIES FALSE)
 endif()
 
 if(NOT DEAL_II_WITH_TRILINOS)
-  message(FATAL_ERROR "deal.II needs to be configured with Trilinos support.")
+  message(SEND_ERROR "deal.II needs to be configured with Trilinos support.")
+  set(DEAL_II_HAS_ALL_DEPENDENCIES FALSE)
+endif()
+
+if(NOT DEAL_II_HAS_ALL_DEPENDENCIES)
+  message(FATAL_ERROR "deal.II wasn't configured with all required dependencies.")
 endif()
