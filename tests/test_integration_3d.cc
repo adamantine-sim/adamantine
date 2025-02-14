8b8806e5b8c1f2a6a6ee2b30c140e8e374bde63e
AshGannon <gannonar@ornl.gov>
2025-02-14 15:31:26 -0500

    testing flags

--- tests/test_integration_3d.cc
+++ tests/test_integration_3d.cc
@@ -92,6 +92,84 @@ BOOST_AUTO_TEST_CASE(integration_3D, *utf::tolerance(0.1))
     }
   }
 }
+/////////////////
+BOOST_AUTO_TEST_CASE(integration_3D, *utf::tolerance(0.1))
+{
+  MPI_Comm communicator = MPI_COMM_WORLD;
+
+  std::vector<adamantine::Timer> timers;
+  initialize_timers(communicator, timers);
+
+  // Read the input.
+  std::string const filename = "demo_316_short.info";
+  adamantine::ASSERT_THROW(std::filesystem::exists(filename) == true,
+                           "The file " + filename + " does not exist.");
+  boost::property_tree::ptree database;
+  boost::property_tree::info_parser::read_info(filename, database);
+
+  auto [temperature, displacement] =
+      run<3, 4, adamantine::SolidLiquidPowder, dealii::MemorySpace::Host>(
+          communicator, database, timers);
+
+  int num_ranks = 0;
+  MPI_Comm_size(communicator, &num_ranks);
+
+  // Limits for a weak non-pointwise check
+  double max_expected = 500.0;
+  double min_expected = 285.0;
+
+  if (num_ranks == 1)
+  {
+    std::ifstream gold_file("integration_3d_gold_short.txt");
+
+    for (unsigned int i = 0; i < temperature.locally_owned_size(); ++i)
+    {
+      BOOST_CHECK(temperature.local_element(i) > min_expected);
+      BOOST_CHECK(temperature.local_element(i) < max_expected);
+
+      double gold_value = -1.;
+      gold_file >> gold_value;
+      BOOST_TEST(temperature.local_element(i) == gold_value);
+    }
+  }
+  else if (num_ranks == 2)
+  {
+    int rank = 0;
+    MPI_Comm_rank(communicator, &rank);
+
+    // To write the gold file
+    
+    std::ofstream gold_file_writer("integration_3d_gold_short_" +
+                                   std::to_string(rank) + ".txt");
+    for (unsigned int i = 0; i < temperature.locally_owned_size(); ++i)
+      gold_file_writer << temperature.local_element(i) << " ";
+
+    gold_file_writer.close();
+    
+
+    std::ifstream gold_file("integration_3d_gold_short_" + std::to_string(rank) +
+                            ".txt");
+
+    for (unsigned int i = 0; i < temperature.locally_owned_size(); ++i)
+    {
+      BOOST_CHECK(temperature.local_element(i) > min_expected);
+      BOOST_CHECK(temperature.local_element(i) < max_expected);
+
+      double gold_value = -1.;
+      gold_file >> gold_value;
+      BOOST_TEST(temperature.local_element(i) == gold_value);
+    }
+  }
+  else
+  {
+    // Only the weaker non-pointwise check for more than 2 cores
+    for (unsigned int i = 0; i < temperature.locally_owned_size(); ++i)
+    {
+      BOOST_CHECK(temperature.local_element(i) > min_expected);
+      BOOST_CHECK(temperature.local_element(i) < max_expected);
+    }
+  }
+}
 
 BOOST_AUTO_TEST_CASE(integration_3D_checkpoint_restart)
 {
