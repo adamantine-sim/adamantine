4e0f6811624595eca8fc3642da9bfcf8412fff2d
John <colemanjs@ornl.gov>
2025-11-17 16:16:23 +0000

    fixup

--- source/validate_input_database.cc
+++ source/validate_input_database.cc
@@ -364,7 +364,7 @@ void validate_input_database(boost::property_tree::ptree &database)
                      boost::iequals(beam_type, "gaussian"),
                  "Beam type, '" + beam_type +
                      "', is not recognized. Valid options are: 'goldak', "
-                     "'electron_beam', 'cube', and gaussian.");
+                     "'electron_beam', 'cube', and 'gaussian'.");
     ASSERT_THROW(database.get_child("sources")
                          .get_child("beam_" + std::to_string(beam_index))
                          .count("scan_path_file") != 0,
