7874bb43e9b6968868b3c5030b5db5468fe611ef
John <colemanjs@ornl.gov>
2025-11-17 16:15:07 +0000

    fixup

--- source/ThermalPhysics.templates.hh
+++ source/ThermalPhysics.templates.hh
@@ -5,7 +5,6 @@
 #ifndef THERMAL_PHYSICS_TEMPLATES_HH
 #define THERMAL_PHYSICS_TEMPLATES_HH
 
-#include <GaussianHeatSource.hh>
 #include <CubeHeatSource.hh>
 #include <ElectronBeamHeatSource.hh>
 #include <GoldakHeatSource.hh>
@@ -401,11 +400,6 @@ ThermalPhysics<dim, n_materials, p_order, fe_degree, MaterialStates,
     else if (type == "cube")
     {
       _heat_sources[i] = std::make_shared<CubeHeatSource<dim>>(
-          beam_database, units_optional_database);
-    }
-    else if (type == "gaussian")
-    {
-      _heat_sources[i] = std::make_shared<GaussianHeatSource<dim>>(
           beam_database, units_optional_database);
     }
     else
