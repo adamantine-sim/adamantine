3d7451450272b53fdfd04ac79bd0c9c70c3705e9
John <colemanjs@ornl.gov>
2026-04-17 13:19:08 -0400

    Rebase from main

--- source/GaussianHeatSource.cc
+++ source/GaussianHeatSource.cc
@@ -1,10 +1,12 @@
-/* SPDX-FileCopyrightText: Copyright (c) 2020 - 2025, the adamantine authors.
+/* SPDX-FileCopyrightText: Copyright (c) 2020 - 2026, the adamantine authors.
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  */
 
 #include <GaussianHeatSource.hh>
 #include <instantiation.hh>
 #include <types.hh>
+
+#include <deal.II/base/utilities.h>
 
 namespace adamantine
 {
@@ -14,9 +16,9 @@ GaussianHeatSource<dim>::GaussianHeatSource(
     boost::property_tree::ptree const &beam_database,
     boost::optional<boost::property_tree::ptree const &> const
         &units_optional_database)
-    : HeatSource<dim>(beam_database, units_optional_database)
+    : HeatSource<dim>(beam_database, units_optional_database),
+      _five_axis(this->_scan_path.is_five_axis())
 {
-  // Read the mandatory empirical coefficients for the Gaussian model.
   _A = beam_database.get<double>("A");
   _B = beam_database.get<double>("B");
 }
@@ -24,88 +26,97 @@ GaussianHeatSource<dim>::GaussianHeatSource(
 template <int dim>
 void GaussianHeatSource<dim>::update_time(double time)
 {
-  // Get the current beam center from the scan path
-  dealii::Point<3> const &path = this->_scan_path.value(time);
-  for (unsigned int d = 0; d < 3; ++d)
-  {
-    _beam_center(d) = path(d);
-  }
+  double const segment_power_modifier =
+      this->_scan_path.get_power_modifier(time);
+  this->_source_on = (segment_power_modifier > 0.0);
 
-  // Update the beam dimensions
+  dealii::Point<3> const &path = this->_scan_path.value(time);
+  _quaternion = this->_scan_path.get_current_quaternion();
+
+  for (unsigned int d = 0; d < 3; ++d)
+    _beam_center(d) = path(d);
+
   _depth = this->_beam.depth;
   _radius_squared = this->_beam.radius_squared;
 
-  // Calculate the shape factor 'k' based on the beam aspect ratio.
-  double const aspect_ratio = std::max(1.0, _depth[0] / this->_beam.radius); // TODO: check
-  double const n = std::min(std::max(0.0, _A * std::log2(aspect_ratio) + _B), 9.0);
+  double const aspect_ratio = std::max(1.0, _depth[0] / this->_beam.radius);
+  double const n =
+      std::min(std::max(0.0, _A * std::log2(aspect_ratio) + _B), 9.0);
   _k = std::pow(2.0, n);
-  
-  // Calculate the effective volume (V0) of the heat source.
-  // This is the integral of the weight function and is used for normalization.
-  auto const V0 = 0.5 * dealii::numbers::PI * _radius_squared * _depth *
-                  std::tgamma(1.0 / _k) /
-                  (_k * std::pow(3.0, 1.0 / _k));
 
-  double segment_power_modifier = this->_scan_path.get_power_modifier(time);
-  double const effective_power = this->_beam.absorption_efficiency *
-                                 this->_beam.max_power *
-                                 segment_power_modifier;
-                                 
-  // Normalize alpha to ensure total power equals the integral of the heat source
+  double const V0 =
+      0.5 * dealii::numbers::PI * _radius_squared[0] * _depth[0] *
+      std::tgamma(1.0 / _k) / (_k * std::pow(3.0, 1.0 / _k));
+
+  double const effective_power =
+      this->_beam.absorption_efficiency * this->_beam.max_power *
+      segment_power_modifier;
+
   _alpha = effective_power / V0;
 }
 
 template <int dim>
-double GaussianHeatSource<dim>::value(dealii::Point<dim> const &point,
-                                    double const height) const
+double GaussianHeatSource<dim>::value(dealii::Point<dim> const &point) const
 {
-  // Get the reference depth of the beam
-  double const z = point[axis<dim>::z] - height;
-  if ((z + this->_beam.depth) < 0.)
+  dealii::Point<dim> rotated_point;
+  if constexpr (dim == 2)
   {
-    return 0.;
+    rotated_point = point;
   }
-  
-  double xpy_squared =
-      std::pow(point[axis<dim>::x] - _beam_center[axis<dim>::x][0], 2);
-
-  if (dim == 3)
+  else
   {
-    xpy_squared +=
-        std::pow(point[axis<dim>::y] - _beam_center[axis<dim>::y][0], 2);
+    rotated_point = _five_axis ? _quaternion.rotate(point) : point;
   }
 
-  // Optimization: return zero if the heat source contribution will be negligible.
-  if (xpy_squared > 5. * this->_beam.radius_squared)
+  double const z = rotated_point[axis<dim>::z] - _beam_center[axis<dim>::z][0];
+  if ((z + _depth[0]) < 0.)
   {
     return 0.;
   }
-  if (std::abs(z) > 5. * this->_beam.depth)
+
+  double xpy_squared = dealii::Utilities::fixed_power<2>(
+      rotated_point[axis<dim>::x] - _beam_center[axis<dim>::x][0]);
+  if constexpr (dim == 3)
+  {
+    xpy_squared += dealii::Utilities::fixed_power<2>(
+        rotated_point[axis<dim>::y] - _beam_center[axis<dim>::y][0]);
+  }
+
+  if (xpy_squared > 5. * _radius_squared[0])
   {
     return 0.;
   }
-  
-  // Radial component
+  if (std::abs(z) > 5. * _depth[0])
+  {
+    return 0.;
+  }
+
   double const radial_component =
-      std::exp(-2.0 * xpy_squared / this->_radius_squared[0]); // TODO: check
+      std::exp(-2.0 * xpy_squared / _radius_squared[0]);
 
-  // Depth component
   double const depth_component =
-      std::exp(-3.0 * std::pow(std::abs(z / this->_beam.depth), _k));
- 
-  // Heat source equation  
+      std::exp(-3.0 * std::pow(std::abs(z) / _depth[0], _k));
+
   return _alpha[0] * radial_component * depth_component;
 }
 
 template <int dim>
 dealii::VectorizedArray<double> GaussianHeatSource<dim>::value(
-    dealii::Point<dim, dealii::VectorizedArray<double>> const &points,
-    dealii::VectorizedArray<double> const &height) const
+    dealii::Point<dim, dealii::VectorizedArray<double>> const &points) const
 {
-  auto const z = points[axis<dim>::z] - height;
+  dealii::Point<dim, dealii::VectorizedArray<double>> rotated_points;
+  if constexpr (dim == 2)
+  {
+    rotated_points = points;
+  }
+  else
+  {
+    rotated_points = _five_axis ? _quaternion.rotate(points) : points;
+  }
+
+  auto const z = rotated_points[axis<dim>::z] - _beam_center[axis<dim>::z];
   auto const z_depth = z + _depth;
-  
-  // Optimization: create a mask for points where the heat source is negligible (depth)
+
   dealii::VectorizedArray<double> depth_mask;
   for (unsigned int i = 0; i < depth_mask.size(); ++i)
   {
@@ -116,16 +127,15 @@ dealii::VectorizedArray<double> GaussianHeatSource<dim>::value(
     return depth_mask;
   }
 
-  auto xpy_squared = points[axis<dim>::x] - _beam_center[axis<dim>::x];
-  xpy_squared *= xpy_squared;
+  auto xpy_squared = dealii::Utilities::fixed_power<2>(
+      rotated_points[axis<dim>::x] - _beam_center[axis<dim>::x]);
   if constexpr (dim == 3)
   {
-    auto y_squared = points[axis<dim>::y] - _beam_center[axis<dim>::y];
-    y_squared *= y_squared;
+    auto const y_squared = dealii::Utilities::fixed_power<2>(
+        rotated_points[axis<dim>::y] - _beam_center[axis<dim>::y]);
     xpy_squared += y_squared;
   }
 
-  // Optimization: create a mask for points where the heat source is negligible (radial)
   auto const xpy_area = 5. * _radius_squared - xpy_squared;
   dealii::VectorizedArray<double> xpy_mask;
   for (unsigned int i = 0; i < xpy_mask.size(); ++i)
@@ -137,23 +147,21 @@ dealii::VectorizedArray<double> GaussianHeatSource<dim>::value(
     return xpy_mask;
   }
 
-  // Gaussian heat source equation:
-  // alpha * exp(-3*(xpy_squared/radius_squared + (z/depth)^2))
-  auto const radial_component =
-    std::exp(-2.0 * xpy_squared / _radius_squared);
-
   dealii::VectorizedArray<double> shape = _k;
+
+  auto const radial_component =
+      std::exp(-2.0 * xpy_squared / _radius_squared);
 
   auto const depth_component =
       std::exp(-3.0 * std::pow(std::abs(z / _depth), shape));
-  
+
   return depth_mask * _alpha * radial_component * depth_component;
 }
 
 template <int dim>
 dealii::BoundingBox<dim>
 GaussianHeatSource<dim>::get_bounding_box(double const time,
-                                        double const scaling_factor) const
+                                          double const scaling_factor) const
 {
   dealii::Point<3> const &beam_center = this->_scan_path.value(time, false);
   if constexpr (dim == 2)
@@ -165,12 +173,38 @@ GaussianHeatSource<dim>::get_bounding_box(double const time,
   }
   else
   {
-    return {{{beam_center[axis<dim>::x] - scaling_factor * this->_beam.radius,
-              beam_center[axis<dim>::y] - scaling_factor * this->_beam.radius,
-              beam_center[axis<dim>::z] - scaling_factor * this->_beam.depth},
-             {beam_center[axis<dim>::x] + scaling_factor * this->_beam.radius,
-              beam_center[axis<dim>::y] + scaling_factor * this->_beam.radius,
-              beam_center[axis<dim>::z]}}};
+    dealii::Point<3> max_corner(
+        beam_center[axis<dim>::x] + scaling_factor * this->_beam.radius,
+        beam_center[axis<dim>::y] + scaling_factor * this->_beam.radius,
+        beam_center[axis<dim>::z]);
+    dealii::Point<3> min_corner(
+        beam_center[axis<dim>::x] - scaling_factor * this->_beam.radius,
+        beam_center[axis<dim>::y] - scaling_factor * this->_beam.radius,
+        beam_center[axis<dim>::z] - scaling_factor * this->_beam.depth);
+
+    if (this->_scan_path.is_five_axis())
+    {
+      dealii::Point<3> rotated_max_corner =
+          this->_scan_path.rotate(time, max_corner);
+      dealii::Point<3> rotated_min_corner =
+          this->_scan_path.rotate(time, min_corner);
+
+      dealii::Point<3> new_max_corner;
+      dealii::Point<3> new_min_corner;
+      for (int d = 0; d < 3; ++d)
+      {
+        new_max_corner[d] =
+            std::max(rotated_max_corner[d], rotated_min_corner[d]);
+        new_min_corner[d] =
+            std::min(rotated_max_corner[d], rotated_min_corner[d]);
+      }
+
+      return {{new_min_corner, new_max_corner}};
+    }
+    else
+    {
+      return {{min_corner, max_corner}};
+    }
   }
 }
 
